efc0d2effc608b8c460f5865367da6ebd0a76a41
Mattéo Delabre ✏ <contact@matteodelabre.me>
2016-08-03 17:38:50 +0200

    Benchmark with libxmljs and node-expat

--- benchmark/index.js
+++ benchmark/index.js
@@ -7,8 +7,10 @@ const path = require('path');
 
 // benchmarked libraries:
 const Saxophone = require('../dist');
-const sax = require('sax');
 const EasySax = require('easysax');
+const expat = require('node-expat');
+const libxmljs = require('libxmljs');
+const sax = require('sax');
 
 // test file:
 const xml = fs.readFileSync(path.join(__dirname, 'fixture.xml')).toString();
@@ -16,59 +18,29 @@ const xml = fs.readFileSync(path.join(__dirname, 'fixture.xml')).toString();
 (new Benchmark.Suite)
     .add('Saxophone', () => {
         const parser = Saxophone();
-        let counter = 0;
-
-        parser.on('tagopen', () => {
-            counter += 1;
-        });
-
-        parser.on('tagclose', () => {
-            counter += 1;
-        });
-
-        parser.on('text', () => {
-            counter += 1;
-        });
-
         parser.parse(xml);
-    })
-    .add('sax', () => {
-        const parser = sax.parser(false);
-        let counter = 0;
-
-        parser.onopentag = () => {
-            counter += 1;
-        };
-
-        parser.onclosetag = () => {
-            counter += 1;
-        };
-
-        parser.ontext = () => {
-            counter += 1;
-        };
-
-        parser.write(xml).close();
     })
     .add('EasySax', () => {
         const parser = new EasySax();
-        let counter = 0;
 
-        parser.on('startNode', (elem, attr) => {
-            attr();
-            counter += 1;
-        });
-
-        parser.on('endNode', () => {
-            counter += 1;
-        });
-
-        parser.on('textNode', () => {
-            counter += 1;
-        });
+        // force EasySax to parse the attributes
+        parser.on('startNode', (elem, attr) => attr());
 
         parser.parse(xml);
     })
+    .add('node-expat', () => {
+        const parser = new expat.Parser('UTF-8');
+        parser.write(xml);
+    })
+    .add('libxmljs.SaxParser', () => {
+        const parser = new libxmljs.SaxParser();
+        parser.parseString(xml);
+    })
+    .add('sax-js', () => {
+        const parser = sax.parser(false);
+        parser.write(xml).close();
+    })
+
     .on('cycle', ev => console.log(ev.target.toString()))
     .on('complete', function () {
         console.log('Fastest is ' + this.filter('fastest').map('name'));
